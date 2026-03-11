78137cd06b208d2f1904d14b28266146b288cd72
Aldiyar <nesquikyey@gmail.com>
2026-03-11 11:33:49 +0500

    fixes

--- middleware.ts
+++ middleware.ts
@@ -47,5 +47,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|_faces|_worker-snaps).*)"],
 };
